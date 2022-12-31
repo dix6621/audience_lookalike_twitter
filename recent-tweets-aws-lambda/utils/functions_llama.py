b0dc3075e53ddaf044411ead23cd672c8c8bcb43
dix6621 <120443007+dix6621@users.noreply.github.com>
2022-12-31 13:31:47 +0800

    Delete functions_llama.py

--- recent-tweets-aws-lambda/utils/functions_llama.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def llama_pools_yield():
-    response = requests.get('https://yields.llama.fi/pools').json()
-    return response
-    
-def llama_protocols():
-    response = requests.get('https://api.llama.fi/protocols').json()
-    return response
